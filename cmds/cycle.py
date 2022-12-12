abc0d6b143ac4c77b6f3fe60131ffc363879d5bf
ClemenPine <ClemenPine@pm.me>
2022-12-11 20:06:20 -0500

    new cycle command

---
+++ cmds/cycle.py
@@ -0,0 +1,27 @@
+from discord import Message
+
+from util import layout, memory, parser
+
+def exec(message: Message):
+    args = parser.get_args(message)
+
+    ll = memory.get(args[0])
+    if not ll:
+        return f'Error: couldn\'t find any layout named `{args[0]}`'
+
+    for cycle in args[1:]:
+        cmap = dict(zip(cycle, cycle[1:] + cycle[0]))
+        keymap = {k: ll['keys'][k] for k in cycle}
+
+        for key, val in cmap.items():
+            ll['keys'][key] = keymap[val]  
+
+    ll['name'] += ' (modified)'
+
+    return layout.to_string(ll)
+
+def use():
+    return 'cycle [layout_name] [chars]'
+
+def desc():
+    return 'cycle a layout\'s letters around'
