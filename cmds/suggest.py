25dadcf288f697766bcf4bd06e0b9a9a2c896a56
ClemenPine <ClemenPine@pm.me>
2022-12-28 18:11:10 -0500

    suggestion command

---
+++ cmds/suggest.py
@@ -0,0 +1,11 @@
+from discord import Message
+
+from util import parser
+
+def exec(message: Message):
+    suggestion = parser.get_arg(message)
+
+    with open('suggestions.txt', 'a') as f:
+        f.write(f'\n{message.author.name}: {suggestion}')
+
+    return 'Received :)'
