0388d89f30a1686072014f948ea72ccff9035c39
ClemenPine <ClemenPine@pm.me>
2022-12-28 20:59:43 -0500

    likes command

---
+++ cmds/likes.py
@@ -0,0 +1,24 @@
+import json
+from discord import Message
+
+def exec(message: Message):
+    id = message.author.id
+    name = message.author.name
+
+    with open('likes.json', 'r') as f:
+        likes: dict[str, list[int]] = json.load(f)
+
+    lines = []
+    for layout, ids in likes.items():
+        if id in ids:
+            lines.append(f' - {layout}')
+
+    return '\n'.join(['```', f'{name}\'s liked layouts:'] + lines + ['```'])
+
+
+
+def use():
+    return 'likes'
+
+def desc():
+    return 'see a list of your liked layouts'
