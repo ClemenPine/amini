fbb64ce95ddf049f74a6fbd6159c802ed38b36da
ClemenPine <ClemenPine@pm.me>
2022-12-29 14:00:11 -0500

    homerow command

---
+++ cmds/homerow.py
@@ -0,0 +1,37 @@
+import glob
+import json
+import random
+from discord import Message
+
+from util import parser
+
+def exec(message: Message):
+    row = parser.get_arg(message)
+
+    lines = []
+    for file in glob.glob('layouts/*.json'):
+        with open(file, 'r') as f:
+            ll = json.load(f)
+
+        homerow = ''.join(k for k in ll['keys'] if ll['keys'][k]['row'] == 1)
+        if (
+            row in homerow or
+            row in homerow[::-1]
+        ):
+            lines.append(ll['name'])
+
+    if len(lines) < 25:
+        res = lines
+    else:
+        res = random.sample(lines, k=25)
+
+    res = list(sorted(res, key=lambda x: x.lower()))
+
+    return '\n'.join([f'I found {len(lines)} matches, here are a few', '```'] + res + ['```'])
+
+
+def use():
+    return 'homerow [string]'
+
+def desc():
+    return 'search for layouts with a particular string in homerow'
