ba940fe1326ed534d9197849659e8c8aefd30811
ClemenPine <ClemenPine@pm.me>
2023-01-04 18:37:10 -0500

    add typeracer quotes

--- scripts/data.py
+++ scripts/data.py
@@ -3,7 +3,7 @@ import json
 from collections import Counter
 from more_itertools import windowed
 
-FILE = 'english-450k'
+FILE = 'tr-quotes'
 NGRAMS = ['monograms', 'bigrams', 'trigrams']
 
 def ngrams(text: str, *, n: int):
