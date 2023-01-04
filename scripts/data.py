cc057f339517da2fbb546ca8e70b9bbf43aa09c5
ClemenPine <ClemenPine@pm.me>
2023-01-04 17:03:38 -0500

    add shai corpus

--- scripts/data.py
+++ scripts/data.py
@@ -3,33 +3,30 @@ import json
 from collections import Counter
 from more_itertools import windowed
 
-FILE_NAME = 'mt-quotes'
-
+FILE = 'shai'
 NGRAMS = ['monograms', 'bigrams', 'trigrams']
 
+def ngrams(text: str, *, n: int):
+    grams = windowed(text, n=n)
+    return dict(Counter(''.join(x) for x in grams if not ' ' in x).most_common())
+
+
 def main():
-    file = f'corpora/{FILE_NAME}.txt'
-    cache = f'cache/{FILE_NAME}'
+    with open(f'corpora/{FILE}.txt', 'r') as f:
+        text = ' '.join(f.read().split())
 
-    os.mkdir(cache)
+    if not os.path.exists(FILE):
+        os.mkdir(FILE)
 
-    with open(file, 'r') as f:
-        words = f.read().split()
-
-    # write ngrams
     for n in range(3):
-        grams = [''.join(x) for x in windowed(' '.join(words), n=n+1)]
-        grams = [x for x in grams if not ' ' in x]
+        res = ngrams(text, n=n+1)
+        with open(f'{FILE}/{NGRAMS[n]}.json', 'w') as f:
+            json.dump(res, f, indent=4)
 
-        res = dict(Counter(grams).most_common())
+    res = dict(Counter(text.split()).most_common())
+    with open(f'{FILE}/words.json', 'w') as f:
+        json.dump(res, f, indent=4)
 
-        with open(f'{cache}/{NGRAMS[n]}.json', 'w') as f:
-            f.write(json.dumps(res, indent=4))
 
-    # write words
-    res = dict(Counter(words).most_common())
-    with open(f'{cache}/words.json', 'w') as f:
-        f.write(json.dumps(res, indent=4))
-        
 if __name__ == '__main__':
     main()
