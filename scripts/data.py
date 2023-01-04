aa45454800853e46fa97dbc4d8eb4c5d9f52b491
ClemenPine <ClemenPine@pm.me>
2023-01-04 18:28:45 -0500

    add more corpora

--- scripts/data.py
+++ scripts/data.py
@@ -3,7 +3,7 @@ import json
 from collections import Counter
 from more_itertools import windowed
 
-FILE = 'shai'
+FILE = 'english-450k'
 NGRAMS = ['monograms', 'bigrams', 'trigrams']
 
 def ngrams(text: str, *, n: int):
