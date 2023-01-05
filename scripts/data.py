8a5feda16420fc211f52faa19407d82b01cdef4d
ClemenPine <ClemenPine@pm.me>
2023-01-04 19:19:11 -0500

    recalculate akl corpus

--- scripts/data.py
+++ scripts/data.py
@@ -3,7 +3,7 @@ import json
 from collections import Counter
 from more_itertools import windowed
 
-FILE = 'keymash'
+FILE = 'akl'
 NGRAMS = ['monograms', 'bigrams', 'trigrams']
 
 def ngrams(text: str, *, n: int):
