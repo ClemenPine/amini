75324632b9e73a3387699a6e227b5426037c6f93
ClemenPine <ClemenPine@pm.me>
2023-01-04 22:48:48 -0500

    more stats

--- cmds/stats.py
+++ cmds/stats.py
@@ -1,5 +1,6 @@
 import json
 import glob
+from collections import Counter
 from discord import Message
 
 def exec(message: Message):
@@ -11,7 +12,11 @@ def exec(message: Message):
     with open('likes.json', 'r') as f:
         likes = json.load(f)
 
+    with open('corpora.json', 'r') as f:
+        corpora = json.load(f)
+
     most_liked = list(sorted(likes.items(), key=lambda x: len(x[1]), reverse=True))
+    top_corpora = Counter(corpora.values()).most_common()
 
     return '\n'.join([
         '```',
@@ -25,6 +30,11 @@ def exec(message: Message):
         f'    {most_liked[2][0]:<15} ({len(likes[most_liked[2][0]])} likes)',
         f'    {most_liked[3][0]:<15} ({len(likes[most_liked[3][0]])} likes)',
         f'    {most_liked[4][0]:<15} ({len(likes[most_liked[4][0]])} likes)',
+        '',
+        f'Top Corpora:',
+        f'    {top_corpora[0][0]:<15} ({top_corpora[0][1]} users)',
+        f'    {top_corpora[1][0]:<15} ({top_corpora[1][1]} users)',
+        f'    {top_corpora[2][0]:<15} ({top_corpora[2][1]} users)',
         '```'
     ])
 
