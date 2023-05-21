cf8cec648cf50e9976830dcd443f500436584bd8
Apsu <apsu@propter.net>
2023-05-21 11:38:27 +0000

    Allow multiple ngrams

--- cmds/freq.py
+++ cmds/freq.py
@@ -8,23 +8,30 @@ RESTRICTED = False
 
 def exec(message: Message):
 	id = message.author.id
-	ngram = parser.get_arg(message)
+	query = parser.get_args(message)
 
-	lengram = len(ngram)
+	ntype = len(query[0]) if len(query) > 0 else None
 
-	if lengram < 1 or lengram > 3:
-		return "Please specify an ngram between 1-3 chars"
+	if not query or ntype < 1 or ntype > 3:
+		return "Please provide at least 1 ngram between 1-3 chars"
 
-	ngrams = corpora.ngrams(lengram, id=id)
+	if len(query) > 5:
+		return "Please provide no more than 5 ngrams"
+
+	ngrams = corpora.ngrams(ntype, id=id)
 	corpus = corpora.get_corpus(id)
 
-	pattern = re.compile(ngram.replace('.', '\.').replace('_', '.'))
-	number = sum(value for key, value in ngrams.items() if pattern.search(key))
+	count = 0
+	for item in query:
+		if len(item) != ntype:
+			return "All ngrams must be same length"
+		pattern = re.compile(item.replace('.', '\.').replace('_', '.'))
+		count += sum(value for key, value in ngrams.items() if pattern.search(key))
 	
-	if number == 0:
-		return f"`{ngram}` not found in corpus `{corpus}`"
+	if count == 0:
+		return f"`{' '.join(query)}` not found in corpus `{corpus}`"
 
 	total = sum(ngrams.values())
 
-	return f"`{ngram}` occurs in {number / total:.2%} of `{corpus}`"
+	return f"`{' '.join(query)}` occur in {count / total:.2%} of `{corpus}`"
 	
