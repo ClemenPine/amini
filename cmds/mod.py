a4c76942f4463645f962ed56818a6a6b555b19d0
Waterdragen <96000090+Waterdragen@users.noreply.github.com>
2023-10-14 02:05:30 +0800

    Refactor `get_kwargs`

--- cmds/mod.py
+++ cmds/mod.py
@@ -1,5 +1,3 @@
-import argparse
-
 from discord import Message
 from importlib import import_module
 
@@ -9,19 +7,30 @@ from util.consts import JSON
 RESTRICTED = False
 
 def exec(message: Message):
-    command = parser.get_arg(message)
+    kwargs = parser.get_kwargs(message, str, angle=bool, unangle=bool, mirror=bool, cycle=list, swap=list)
+    layout_name = kwargs['args']
+    if not layout_name:
+        kw_tips = """
+                Usage: `mod layout_name [--kwarg1, --kwarg2, ...]`
+                ```
+                --angle:
+                    view the angle modded version of a layout
+                --unangle:
+                    view the non angle modded version of a layout
+                --mirror:
+                    view the mirrored version of a layout
+                --cycle:
+                    cycle a layout's letters around
+                --swap:
+                    alias of --cycle
+                ```
+                """
+        return kw_tips.replace(' ' * 16, '').strip()
 
-    if not command:
-        kw_tips = ['Usage: `mod layout_name [--kwarg1, --kwarg2, ...]`', '```']
-        kw_tips.extend(f'--{kw}:\n    {__get_layout_desc(kw)}' for kw in __Parser.keywords.keys() if kw != 'swap')
-        kw_tips.extend(['--swap:\n    alias of --cycle', '```'])
-        return '\n'.join(kw_tips)
-    kwargs = __Parser.get_kwargs(command)
-
-    ll = memory.find(kwargs['layout_name'].lower())
+    ll = memory.find(layout_name.lower())
 
     if not ll:
-        return f'Error: could not find layout `{kwargs["layout_name"]}`'
+        return f'Error: could not find layout `{layout_name}`'
     
     if kwargs['angle'] and kwargs['unangle']:
         kwargs['angle'] = False  # `--angle --unangle` defaults to unanglemodded
@@ -58,27 +67,4 @@ def __get_layout_desc(mode: str) -> str:
     mod = import_module(f'cmds.{mode}')
     return mod.desc()
 
-class __Parser:
-    keywords: dict[str, dict] = {
-        'angle': {'action': 'store_true'},
-        'unangle': {'action': 'store_true'},
-        'mirror': {'action': 'store_true'},
-        'cycle': {'nargs': '*', 'default': None},
-        'swap': {'nargs': '*', 'default': None}
-    }
-    parser = argparse.ArgumentParser()
-    parser.add_argument('layout_name', nargs='*')
-    for keyword, arg_config in keywords.items():
-        parser.add_argument(f'--{keyword}', **arg_config)
-
-    @classmethod
-    def get_kwargs(cls, command: str) -> dict[str, str | bool | list[str]]:
-        command: list[str] = command.split()
-        kwargs, unknown_args = cls.parser.parse_known_args(command)
-        kwargs.layout_name = " ".join(kwargs.layout_name)
-        if kwargs.cycle is None:
-            kwargs.cycle = []
-        if kwargs.swap is None:
-            kwargs.swap = []
-        return vars(kwargs)
 
