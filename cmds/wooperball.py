0a789efb2b1113b5eebda2d3d9170a4040c69ab8
semi <semilin@disroot.org>
2023-11-18 21:50:43 -0800

    add small chance of :woopwoop: to wooperball

--- cmds/wooperball.py
+++ cmds/wooperball.py
@@ -4,7 +4,8 @@ from discord import Message
 RESTRICTED = False
 
 def exec(message: Message):
-    return random.choice([
+    return random.choices([
         '<a:wooperno:1085582045121613874>',
-        '<a:wooper:1081033714043207771>'
-    ])
+        '<a:wooper:1081033714043207771>',
+        '<:woopwoop:1168675851592798288>'
+    ], weights = [49, 49, 1])[0]
