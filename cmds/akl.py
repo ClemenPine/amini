5eb79d014c3e0da405682e642c66eb4a18ac9ac9
Waterdragen <96000090+Waterdragen@users.noreply.github.com>
2023-11-24 21:56:07 +0800

    Discord role checks with case-insensitivity

--- cmds/akl.py
+++ cmds/akl.py
@@ -1,5 +1,7 @@
-import discord.utils
 from discord import Client, Guild
+from typing import Iterable, TypeVar
+
+_VT = TypeVar('_VT')
 
 AKL_ID = 807843650717483049
 
@@ -44,23 +46,34 @@ LAYOUT_ROLES: list[str] = [
     'Recurva',
 ]
 
+class CaseInsensitiveDict(dict):
+    @classmethod
+    def from_iterable(cls, iterable: Iterable[tuple[str, _VT]]):
+        return cls((k.lower(), i) for (k, i) in iterable)
+
+    def __getitem__(self, key: str):
+        return super().__getitem__(key.lower())
+
+    def __contains__(self, key: str):
+        return super().__contains__(key.lower())
+
 def exec(bot: Client):
     guild: Guild = bot.get_guild(AKL_ID)
     if not guild:
         return 'Error: Cannot find akl server'
 
-    role_counts: dict[str, int] = {}
-    for role_name in LAYOUT_ROLES:
-        role = discord.utils.get(guild.roles, name=role_name)
-        if role:
-            role_counts[role_name] = len(role.members)
+    roles = guild.roles
+    role_counts: dict[str, int] = CaseInsensitiveDict.from_iterable((role.name, len(role.members)) for role in roles)
+    layout_role_counts: list[tuple[str, int]] = [(layout_role, role_counts[layout_role])
+                                                 for layout_role in LAYOUT_ROLES
+                                                 if layout_role in role_counts]
 
-    sorted_role_counts: list[tuple[str, int]] = sorted(role_counts.items(), key=lambda x: x[1], reverse=True)
-    sorted_role_counts = sorted_role_counts[:20]
+    layout_role_counts.sort(key=lambda x: x[1], reverse=True)
+    layout_role_counts = layout_role_counts[:20]
 
     res = ['```',
            '--- AKL STATS ---',
            'Layout role count:']
-    res.extend(f'    {role_name:<15} ({count} users)' for (role_name, count) in sorted_role_counts)
+    res.extend(f'    {role_name:<15} ({count} users)' for (role_name, count) in layout_role_counts)
     res.append('```')
     return '\n'.join(res)
