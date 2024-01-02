1df871075204c849e176ec3cef794ffc8a264378
ddn <107157865+drowningnewt@users.noreply.github.com>
2024-01-02 18:14:40 -0500

    Added UTF-8 encoding

--- scripts/data.py
+++ scripts/data.py
@@ -3,16 +3,15 @@ import json
 from collections import Counter
 from more_itertools import windowed
 
-FILE = 'chained-bigrams'
+FILE = 'french'
 NGRAMS = ['monograms', 'bigrams', 'trigrams']
 
 def ngrams(text: str, *, n: int):
     grams = windowed(text, n=n)
     return dict(Counter(''.join(x) for x in grams if not ' ' in x).most_common())
 
-
 def main():
-    with open(f'corpora/{FILE}.txt', 'r') as f:
+    with open(f'corpora/{FILE}.txt', 'r', encoding='utf-8') as f:
         text = ' '.join(f.read().split())
 
     if not os.path.exists(FILE):
@@ -20,13 +19,12 @@ def main():
 
     for n in range(3):
         res = ngrams(text, n=n+1)
-        with open(f'{FILE}/{NGRAMS[n]}.json', 'w') as f:
-            json.dump(res, f, indent=4)
+        with open(f'{FILE}/{NGRAMS[n]}.json', 'w', encoding='utf-8') as f:
+            json.dump(res, f, indent=4, ensure_ascii=False)
 
     res = dict(Counter(text.split()).most_common())
-    with open(f'{FILE}/words.json', 'w') as f:
-        json.dump(res, f, indent=4)
-
+    with open(f'{FILE}/words.json', 'w', encoding='utf-8') as f:
+        json.dump(res, f, indent=4, ensure_ascii=False)
 
 if __name__ == '__main__':
     main()
