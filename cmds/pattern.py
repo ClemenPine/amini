fe5dee03a8cd6fba5f8a2e94a1e3210ee5d79a70
aoi-buh <matthewcheung1023@gmail.com>
2024-11-18 08:25:21 -0700

    Should be fixed?

--- cmds/pattern.py
+++ cmds/pattern.py
@@ -23,29 +23,30 @@ def exec(message: Message):
     if not all(all(finger in allowed_fingers for finger in node.split('|')) for node in query):
         return "Please provide valid finger values (e.g., LI, _, LI|RR)"
 
-    ngrams = corpora.ngrams(len(query), id=message.author.id)
+    total = 0
     freq = 0
-    total = sum(ngrams.values())
     ngrams = {}
+    fingers = [list(allowed_fingers) if finger == '_' else [finger.split('|')] for finger in query]
 
-    for gram, count in ngrams.items():
+    for gram, count in corpora.ngrams(len(query), id=message.author.id).items():
+        total += count
         gram = gram.lower()
         if len(set(gram)) != len(gram):
             continue
 
-        fingers = [list(allowed_fingers) if finger == "_" else [finger.split("|")] for finger in query]
         keys = [ll.keys[x].finger for x in gram if x in ll.keys]
 
-        if all(key in finger for key, finger in zip(keys,fingers)):
+        if all(key in finger for key, finger in zip(keys, fingers)):
             ngrams[gram] = ngrams.get(gram, 0) + count
             freq += count
 
     ngrams = sorted(ngrams.items(), key=lambda x: x[1], reverse=True)
+    lines = [f'{gram:<6} {count / total:.3%}' for (gram, count), _ in zip(ngrams, range(10))]
 
     return '\n'.join([
         '```',
-        f'Top 10 {ll.name} Patterns for {'-'.join(query)}:'] +
-        [f'{gram:<6} {count / total:.3%}' for (gram, count), _ in zip(ngrams, range(10))] +
+        f'Top {len(lines)} {ll.name} Patterns for {'-'.join(query)}:'] +
+        lines +
         [f'Total {freq / total:.3%}',
         '```'
     ])
